c51e4495c549b0af7ca620805c970447c771d709
7agag <youssef_hagag@icloud.com>
2024-07-13 15:00:39 +0300

    Add project files.

---
+++ day 3/Task1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTi 
+{
+
+    class Student //Task
+    { 
+        int id, age;
+        string name;
+        static Student()
+        {
+            Console.WriteLine("Hello\n");
+
+        }
+        public Student(int id, string name, int age = 18)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public void getdata()
+        {
+            Console.WriteLine($"{id}----{name}----{age}");
+        }
+        public string checkAge() 
+        {
+            if (age >= 24) return $"{name} has been graduated";
+            return $"{name} still student ";
+        }
+    }
+    internal class Task1
+    {
+        //public static void Main(string[] args)
+        //{
+        //    Student std1 = new Student(20,"Mohamed");
+        //    Student std2 = new Student(21,"Youssef",26);
+        //    std1.getdata(); 
+        //    std2.getdata();
+        //    Console.WriteLine(std1.checkAge());
+        //    Console.WriteLine(std2.checkAge());
+
+
+        //}
+    }
+
+}
---
+++ day2/Task1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace iTi
+//{ 
+
+//<<<<<<< HEAD:Task 1 C#/Task1.cs
+    //class Student1()
+    //{
+    //    //name - SSN -Age - phone 
+    //    string name;
+    //    int SSN;
+    //    int Age;
+    //    String phone;
+//=======
+    //class Student()
+    //{
+    //    //name - SSN -Age - phone 
+    //    string name;
+    //    int SSN;
+    //    int Age;
+    //    String phone;
+//>>>>>>> f2986ebf87fe48d5651a058ba008621c46a16c10:Task1.cs
+
+    //    public void getData(int Id, string Name, int age, string pho)
+    //    {
+
+    //    }
+    //    public void print()
+    //    {
+    //    }
+    //}
+    //internal class Task1
+    //{
+    //    public static void Main(string[] args)
+    //    {
+
+
+            //Multiplier Task 1
+            //Console.Write("Enter a Number : ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for (int i=1; i <= 12; i++)//5
+            //{
+
+            //    Console.WriteLine($"{i} * {num} ={i*num}");
+            //}
+            //--------------------------------------------
+
+
+
+
+            //Employee Task2 
+            //Console.Write("Enter your ID :");
+            //int ID = int.Parse(Console.ReadLine());
+            //Console.Write("Enter your name :");
+            //string name = Console.ReadLine();
+            //int Age;
+            //int Salary;
+            //Console.Write("Enter your Age :");
+            //Age = int.Parse(Console.ReadLine());
+            //if (Age < 0 )
+            //{
+            //    do
+            //    {
+            //        Console.Write("Please Enter your Correct Age: ");
+            //        Age = int.Parse(Console.ReadLine());
+            //    } while (Age < 0 );
+            //}
+            //Console.WriteLine("----------------------------\n");
+            //Console.Write("Enter your Salary : ");
+            //Salary = int.Parse(Console.ReadLine());
+            //if (Salary < 0 )
+            //{
+            //    do
+            //    {
+            //        Console.Write("Please Enter your Correct Salary: ");
+            //        Salary = int.Parse(Console.ReadLine());
+            //    } while (Salary < 0 );
+            //}
+            //Console.WriteLine("----------------------------\n");
+
+
+            //Console.WriteLine($"Name : {name}");
+            //Console.WriteLine($"ID   : {ID}");
+            //Console.WriteLine($"Age  : {Age}");
+            //Console.WriteLine($"Salary  : {Salary}");
+
+            //--------------------------------------------
+
+
+
+
+
+            //OOP Task 3
+            //Student1 std = new Student1();          //Instance 
+            //std.getData(122, "Youssef", 20, "+20120541346");
+            //std.print();
+
+
+
+//        }
+//    }
+//}
