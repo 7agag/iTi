40fcc1043faf54d5adbc3a1eeabd381df7c6b2d5
7agag <youssef_hagag@icloud.com>
2024-07-14 15:30:58 +0300

    day4

---
+++ day4/array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTi.day4
+{
+    internal class array
+    {
+        //public static void Main(string[] args)
+        //{
+        //    int x;
+        //    Console.Write("Add length for array : ");
+        //    x = int.Parse(Console.ReadLine());
+
+        //    string[] name = new string[x];
+        //    int[] age = new int[x];
+
+        //    for (int i = 0; i <= x - 1; i++)
+        //    {
+        //        Console.Write("Add name :");
+        //        string s = Console.ReadLine();
+        //        Console.Write("Add age :");
+        //        int y = int.Parse(Console.ReadLine());
+        //        name[i] = s; age[i] = y;
+        //    }
+        //    Console.Write("Find him! Add the lenght of him : ");
+        //    int l = int.Parse(Console.ReadLine());
+
+        //    for (int i = 0; i == l-1; i++)
+        //    {
+
+        //        Console.WriteLine($"name : {name[i]}, age : {age[i]}");
+        //    }
+        //    //name - age
+        //    //user add data
+        //    //user display one form them
+
+        //}
+    }
+}
---
+++ day4/task.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace iTi.day4
+//{
+    //internal class task
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        int product = 1;
+    //        //  1, 2 , 3 , 4        split(", ")
+    //        Console.WriteLine("Enter the Array ");
+    //        string input = Console.ReadLine();
+
+    //        int[] num = input.Split(", ").Select(int.Parse).ToArray();
+
+    //        int[] output = new int[num.Length];
+
+
+    //        for (int i = 0; i < num.Length; i++)
+    //        {
+    //            product = 1;
+    //            for (int j = 0; j < num.Length; j++)
+    //            {
+
+    //                if (i != j) { product *= num[j]; }
+    //            }
+
+    //            Console.WriteLine(product);
+
+    //             output[i] = product;
+    //        }
+
+    //        for (int i = 0; i < output.Length; i++)
+    //         {                 
+    //            Console.Write($"{output[i]} -- "); 
+    //         }
+    //    }
+
+            
+            
+            
+
+    
+    //}
+
+
+//}
