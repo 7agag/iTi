a8f62c7daad6578573cd4672c4946204ad413fc1
7agag <youssef_hagag@icloud.com>
2024-07-20 04:27:48 +0300

    LINQ Task

---
+++ LINQ/helpers.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTi.LINQ
+{
+    public class Transaction { 
+        public DateTime Date {  get; set; }
+        public int Amount { get; set; }
+        public Transaction(DateTime date,int amount)
+        {
+            this.Date = date;
+            this.Amount = amount;
+        }
+    }  
+    public class StudentScores
+    {
+        public int Score { get; set; }
+        public string Sname { get; set; }
+        public string Subject { get; set; }
+        public StudentScores(string Sname, string subject, int score)
+        {
+            this.Score = score;
+            this.Subject = subject;
+            this.Sname = Sname;
+        }
+    }
+
+        public class Book
+    {
+        public string Title {  get; set; }
+        public string Author { get; set; }
+        public Book(string title , string author) {
+
+             Title = title ;
+            Author = author;
+        }
+    }
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public int Salary { get; set; }
+        public string Department {  get; set; }
+        public override string ToString()
+        {
+            return $"{ID} - {FirstName + " " + LastName} - {Salary}";
+        }
+        public Employee(string Firstname,int salary)
+        {
+            FirstName = Firstname;
+            Salary = salary;
+        }
+         public Employee(String Firstname,String Depart)
+         {
+                 this.FirstName = Firstname;
+                 this.Department = Depart;
+         }
+    }
+    public class EMPComparer : IComparer<Employee>
+    {
+        public int Compare(Employee? x, Employee? y)
+        {
+            int comarerResult = x.Salary.CompareTo(y.Salary);
+            if (comarerResult == 0)
+            {
+                return x.FirstName.CompareTo(y.FirstName);
+            }
+            return comarerResult;
+        }
+    }
+    public class EmployeeDto
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; } = null!;
+        public override string ToString()
+        {
+            return $"{ID} - {FullName}";
+        }
+    }
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public List<string> Programming { get; set; } = new();
+
+    }
+    public class Product : IEquatable<Product>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string categore { get; set; } 
+        public decimal Price { get; set; }
+        public Product(string name,string cat,decimal price)
+        {
+            this.Name = name;
+            this.categore = cat;
+            this.Price = price;
+        }
+
+        public bool Equals(Product? other)
+        {
+            if (object.ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            else if (object.ReferenceEquals(this, null))
+            {
+                return false;
+            }
+            else if (this.Id == other.Id && this.Name == other.Name && this.Price == other.Price)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //public override bool Equals(object? obj)
+        //{
+        //    if (object.ReferenceEquals(obj, null))
+        //    {
+        //        return false;
+        //    }
+        //    else if (object.ReferenceEquals(this, null))
+        //    {
+        //        return false;
+        //    }
+        //    else if (obj is Product prod && this.Id == prod.Id && this.Name == prod.Name && this.Price == prod.Price)
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
+        public override int GetHashCode()
+        {
+            int idHashCode = this.Id.GetHashCode();
+            int nameHashCode = this.Name.GetHashCode();
+            int priceHashCode = this.Price.GetHashCode();
+            return idHashCode ^ nameHashCode ^ priceHashCode;
+        }
+    }
+    public interface IPerson
+    {
+        public Guid Identity { get; set; }
+        public string Name { get; set; }
+    }
+    public interface ISkills
+    {
+        public List<string> Skills { get; set; }
+    }
+    public interface IExperiance
+    {
+        public int ExperianceYears { get; set; }
+    }
+    public class Developer : IPerson, ISkills
+    {
+        public Guid Identity { get; set; }
+        public string Name { get; set; } = null!;
+        public List<string> Skills { get; set; } = new();
+    }
+    public class Engineer : IPerson, IExperiance
+    {
+        public Guid Identity { get; set; }
+        public string Name { get; set; } = null!;
+        public int ExperianceYears { get; set; }
+    }
+    public class Fruit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Price { get; set; }
+    }
+    public class FruitComparer : IEqualityComparer<Fruit>
+    {
+        public bool Equals(Fruit? x, Fruit? y)
+        {
+            if (x is null && y is null)
+            {
+                return true;
+            }
+            else if ((x is not null && y is null) || (x is null && y is not null))
+            {
+                return false;
+            }
+            else if (x.Price == y.Price)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Fruit obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            return obj.Price.GetHashCode();
+        }
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Person))
+            {
+                return false;
+            }
+            else if (obj is Person person)
+            {
+                return this.Id == person.Id;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+    public class CollageStudent : IEquatable<CollageStudent>
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime GraduationYear { get; set; }
+        public static List<CollageStudent> GetStudents()
+        {
+            List<CollageStudent> students = new List<CollageStudent>()
+            {
+                new CollageStudent {ID = 101, Name = "yara" ,GraduationYear=new DateTime(2015, 12, 25)},
+                new CollageStudent {ID = 102, Name = "tamer" ,GraduationYear=new DateTime(2016, 12, 25)},
+                new CollageStudent {ID = 101, Name = "YARa",GraduationYear=new DateTime(2015, 12, 25)},
+                new CollageStudent {ID = 104, Name = "ramy",GraduationYear=new DateTime(2018, 12, 25)}
+            };
+            return students;
+        }
+        public bool Equals(CollageStudent other)
+        {
+            if (object.ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return (this.Name.ToLower() == other.Name.ToLower()) && this.ID == other.ID && this.GraduationYear == other.GraduationYear;
+        }
+        public override int GetHashCode()
+        {
+            //invalid to do this => all properties used in equal method should be used in hashcode 
+            int NameHashCode = this.Name == null ? 0 : this.Name.ToLower().GetHashCode();
+            return NameHashCode;
+        }
+    }
+    public class ProductComparer : IEqualityComparer<Product>
+    {
+        public bool Equals(Product? x, Product? y)
+        {
+            if (object.ReferenceEquals(x, null))
+            {
+                return false;
+            }
+            else if (object.ReferenceEquals(y, null))
+            {
+                return false;
+            }
+            else if (x.Id == y.Id && x.Name == y.Name && x.Price == y.Price)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Product obj)
+        {
+            int idHashCode = obj.Id.GetHashCode();
+            int nameHashCode = obj.Name.GetHashCode();
+            int priceHashCode = obj.Price.GetHashCode();
+            return idHashCode ^ nameHashCode ^ priceHashCode;
+        }
+    }
+    public class FruitStatistics
+    {
+        public int MaxPrice { get; set; } = 0;
+        public int MinPrice { get; set; } = 0;
+        public int FruitCount { get; set; } = 0;
+        public int SumPrice { get; set; } = 0;
+        public override string ToString()
+        {
+            return $"MaxPrice = {MaxPrice} , MinPrice = {MinPrice} , SumPrice = {SumPrice} , FruitCount = {FruitCount}";
+        }
+    }
+    public class PrimaryStudent
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int TotalMarks { get; set; }
+        public List<Subject> Subjects { get; set; }
+    }
+    public class Subject
+    {
+        public string SubjectName { get; set; }
+        public int Marks { get; set; }
+    }
+    public class SecondryStudent
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Barnch { get; set; }
+        public int Age { get; set; }
+    }
+    public class SecondryStudentDto
+    {
+        public string BranchName { get; set; }
+        public List<string> StudentNames { get; set; }
+
+    }
+    public class Order
+    {
+
+        public int CustomerId { get; set; }
+        public int OrderId { get; set; }
+        public int Price { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime date {  get; set; }
+
+        public Order(int orderid,int price)
+        {
+            CustomerId = orderid;
+            OrderId = orderid;
+        }
+        public Order(DateTime date,decimal amount)
+        {
+            this.date = date;
+            this.Amount= amount;
+        }
+    }
+    
+    class TestEmployee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+    }
+    public class Sale
+    {
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+
+        public string Name { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        public Sale(string salesman , int amount) {
+        Name=salesman;
+        Amount=amount;
+        }
+    }
+    public class StudentTest
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Branch { get; set; }
+        public int Age { get; set; }
+    }
+    public class EmployeeTest
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int AddressId { get; set; }
+        public int DepartmentId { get; set; }
+        public override string ToString()
+        {
+            return $"{ID} - {Name}";
+        }
+    }
+    public class Address
+    {
+        public int ID { get; set; }
+        public string AddressLine { get; set; }
+    }
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class EmployeeResult
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public override string ToString()
+        {
+            return $"{ID} - {Name} - {Department}";
+        }
+
+    }
+}
---
+++ LINQ/linq_task.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace iTi.LINQ
+{
+    internal class linq_task
+    {
+
+        public static void Main(string[] args)
+        {
+            // 1) Given a list of integers, return the product of all numbers
+            Console.WriteLine("1)Product of numbers");
+            List<int> numbers = new List<int> { 1, 2, 3, 4 };
+            int product = numbers.Aggregate((x, y) => x * y);
+            Console.WriteLine($"The product of numbers = {product}");
+            Console.WriteLine("------------------------------------------");
+
+            // 2)Given a list of strings, return a single string that concatenates all the strings,separated by commas.
+            Console.WriteLine("2)concatenate Strings:");
+            List<string> words = new List<string> { "apple", "banana", "cherry" };
+            var result1 = words.Aggregate((x, y) => x + ',' + y);
+            Console.WriteLine($"'{result1}'");
+            Console.WriteLine("------------------------------------------");
+
+            // 3) Given a list of integers, return a list where each element is the square of corresponding element in the input list.
+            Console.WriteLine("3)Square :");
+            //List<int> numbers = new List<int> { 1, 2, 3, 4 };
+            List<int> square = numbers.Select(x => x * x).ToList();
+            Console.WriteLine(string.Join(",", square));
+            Console.WriteLine("------------------------------------------");
+
+            // 4) Given a list of strings, return a dictionary where the keys are the strings and the values are the lengths of those strings.
+            Console.WriteLine("4) Strings and The value of Length with Dictionary :");
+            //List<string> words = new List<string> { "apple", "banana","cherry" };
+            var result2 = words.ToDictionary(x => x, x => x.Length);
+            Console.WriteLine("{" + string.Join(", ", result2.Select(x => $"'{x.Key}': {x.Value}")) + "}");
+            Console.WriteLine("------------------------------------------");
+
+            // 5) Given a list of integers, return a list where each element is the square of the corresponding element in the input list.
+
+
+            Console.Write("5) The intersect between numbers : ");
+            List<int> list1 = new List<int> { 1, 2, 3, 4 };
+            List<int> list2 = new List<int> { 3, 4, 5, 6 };
+            var intersect = list1.Intersect<int>(list2);
+            Console.WriteLine(string.Join(",", intersect));
+            Console.WriteLine("------------------------------------------");
+
+            // 6) You have a list of employees. Return the name of the employee with the highest salary.
+            Console.WriteLine("6) The name of employee with hightest Salary");
+            List<Employee> employees = new List<Employee>
+            { new Employee("Ali",60000), new Employee("Ramy", 45000), new Employee("Samy", 80000) };
+
+            var emphight = employees.Select(y => y.FirstName).Max();
+            Console.WriteLine(emphight);
+            Console.WriteLine("------------------------------------------");
+
+
+            // 7)You have a list of books. Return a list of books grouped by their authors.
+
+
+            Console.WriteLine("7) Group books with their authors");
+
+            List<Book> books = new List<Book>
+            { new Book("Book1", "Author1"),new Book("Book2", "Author2"), new Book("Book3", "Author1") };
+            var grbooks = books.GroupBy(x => x.Author).ToList();
+
+            foreach (var group in grbooks)
+            {
+                Console.WriteLine($"Author: {group.Key}");
+                foreach (var book in group)
+                {
+                    Console.WriteLine($" ~ {book.Title}");
+                }
+            }
+            Console.WriteLine("------------------------------------------");
+
+
+         // 8) You have a list of sales data. Return the total sales amount for  salesperson.
+
+            Console.WriteLine("8) Total Sales for Salesman");
+            List<Sale> sales = new List<Sale> { new Sale("Ali", 100), new Sale("Ramy", 200), new Sale("Ali", 150) };
+
+            var totalsales = sales.GroupBy(x => x.Name,
+                (name, SaleGroup) => new { Name = name, TotalSales = SaleGroup.Sum(x => x.Amount) }).ToList();
+             foreach (var totalsal in totalsales)
+            {
+                Console.WriteLine($"{totalsal.Name} - {totalsal.TotalSales}");
+            }
+            Console.WriteLine("------------------------------------------");
+
+
+            // 9) You have a list of students with their scores in different subjects
+            // Return highest score for each student.
+            Console.WriteLine(" highest score for each student :\n");
+            List<StudentScores> scores = new List<StudentScores> 
+              { new StudentScores("Ali", "Math", 90),
+                new StudentScores("Ali", "Science", 85), 
+                new StudentScores("Ramy", "Math", 80) };
+            var highestScores = scores.GroupBy(s => s.Sname).Select(g => new { Student = g.Key, HighestScore = g.Max(s => s.Score) });
+
+            Console.WriteLine($"9)The Highest score for each student: {{{string.Join(", ", highestScores.Select(s => $"{s.Student}: {s.HighestScore}"))}}}");
+
+            Console.WriteLine("------------------------------------------");
+
+            // 10)  You have a list of orders with prices. Return the average order price.
+
+            Console.Write ("10)Average of order : ");
+            List<Order> orders = new List<Order> { new Order(101, 50), new Order(102, 200), new Order(103, 150) };
+
+            var avg = orders.Average(o => o.Price);
+            Console.WriteLine(avg);
+
+            Console.WriteLine("------------------------------------------");
+
+            // 11)  Given a list of integers, return a list of tuples where each tuple contains an integer and its frequency in the list
+            Console.Write("11)Frequency of numbers :");
+            List<int> numbers1 = new List<int> { 1, 2, 2, 3, 3, 3 };
+            var frequency = numbers1.GroupBy(x => x).Select(g => new { Number = g.Key, Frequency = g.Count() });
+            Console.WriteLine($" [{string.Join(", ", frequency.Select(f => $"({f.Number}, {f.Frequency})"))}]");
+
+            Console.WriteLine("------------------------------------------");
+
+            // 12)  Given a list of strings, return the first string that contains the letter 'e'.
+            Console.Write("12) String start with 'e' is ");
+
+            List<string> wordss = new List<string> { "cat", "elephant", "dog","tiger" };
+            var firste = wordss.FirstOrDefault(x => x.Contains("e"));
+            Console.WriteLine(firste);
+
+            Console.WriteLine("------------------------------------------");
+
+            // 13)  Given a list of integers, return a list of integers where each integer is multiplied by its index
+            
+            Console.WriteLine("13)each integer is multiplied by its index ");
+
+            //   List<int> numbers = new List<int> { 1, 2, 3, 4 };
+
+            var multi = numbers.Select((number, index) => number * index).ToList();
+
+            Console.WriteLine(string.Join(", ", multi));
+            Console.WriteLine("------------------------------------------");
+
+            // 14) Given a list of strings, return a list of the unique characters in all the strings.
+
+            Console.WriteLine("14)the unique characters in all the strings :");
+
+
+            List<string> words1 = new List<string> { "apple", "banana" };
+
+             var unique = words1.SelectMany(word => word.ToCharArray()).Distinct().ToList();
+
+            Console.WriteLine(string.Join(", ", unique));
+
+            Console.WriteLine("------------------------------------------");
+
+
+            // 15) Given a list of integers, return the count of even and odd numbers
+
+            Console.WriteLine("15)Even and Odd numbers ");
+
+            List<int> numbers2 = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            var evenCount = numbers2.Count(n => n % 2 == 0);
+            var oddCount = numbers2.Count(n => n % 2 != 0);
+
+            Console.WriteLine($"Even: {evenCount}, Odd: {oddCount}");
+
+            Console.WriteLine("------------------------------------------");
+
+
+            // 16) You have a list of employees with their departments. Return the department with the most employees
+            Console.WriteLine("16)the department with the most employees");
+            List<Employee> employees1 = new List<Employee> 
+            { new Employee("Ali", "HR"),
+              new Employee("Ramy", "IT"),
+              new Employee("Samy", "HR"),
+              new Employee("Sara", "IT"),
+              new Employee("John", "IT") };
+
+            var deptemp = employees1.GroupBy(e => e.Department).OrderByDescending(g => g.Count()).First().Key;
+            Console.WriteLine(deptemp);
+
+            Console.WriteLine("------------------------------------------");
+
+            // 17) You have a list of products with their categories and prices. Return the most expensive product in each category
+            Console.WriteLine("17)the most expensive product in each category");
+
+            List<Product> products = new List<Product>
+            { new Product("Laptop","Electronics", 1000),
+              new Product("Phone", "Electronics", 800),
+              new Product("Shirt", "Clothing", 50),
+              new Product("Pants", "Clothing", 60)};
+
+            var exp= products.GroupBy(p => p.categore)
+                .Select(g => g.OrderByDescending(p => p.Price).First()).ToList();
+
+            foreach (var producst in exp)
+            {
+                Console.WriteLine($"{producst.categore}: {producst.Name} - ${producst.Price}");
+            }
+
+            Console.WriteLine("------------------------------------------");
+
+            // 18) You have a list of students with their grades in different subjects. Return the average grade for each student.
+            Console.WriteLine("18)average grade for each student ");
+
+            List<StudentScores> grades = new List<StudentScores> {
+                new StudentScores("Ali", "Math", 90), 
+                new StudentScores("Ali", "Science",85), 
+                new StudentScores("Ramy", "Math", 80),
+                new StudentScores("Ramy","Science", 70) };
+
+            var avggrade = grades.GroupBy(g => g.Sname)
+                .Select(g => new { Student = g.Key, AverageGrade = g.Average(s => s.Score) }).ToList();
+
+            foreach (var item in avggrade)
+            {
+                Console.WriteLine($"Student: {item.Student}, Average Grade: {item.AverageGrade}");
+            }
+
+
+            Console.WriteLine("------------------------------------------");
+
+            // 19) You have a list of transactions with their amounts and dates. Return the total transaction amount for each month.
+
+            Console.WriteLine("19)the total transaction amount for each month :");
+
+            List<Transaction> transactions = new List<Transaction> 
+            { new Transaction(new DateTime(2024, 1, 10), 100),
+              new Transaction(new DateTime(2024, 1, 20), 200), 
+              new Transaction(new DateTime(2024, 2, 5),150) };
+
+            var trans = transactions.GroupBy(t => new { t.Date.Year, t.Date.Month })
+                .Select(g => new { Month = $"{g.Key.Year}-{g.Key.Month}", TotalAmount = g.Sum(t => t.Amount) }).ToList();
+
+            foreach (var item in trans)
+            {
+                Console.WriteLine($"Month: {item.Month}, Total Amount: {item.TotalAmount}");
+            }
+
+            Console.WriteLine("------------------------------------------");
+
+
+
+
+            // 20) You have a list of orders with their order dates and amounts. Return the order amounts for the last 7 days.
+
+
+            Console.WriteLine("19)the order amounts for the last 7 days:");
+            List<Order> orders1 = new List<Order> 
+            { new Order(new DateTime(2024,7, 10), 50), 
+             new Order(new DateTime(2024, 7, 15), 200), 
+             new Order(new DateTime(2024, 7, 16), 150) };
+
+
+            DateTime lastWeek = DateTime.Now.AddDays(-7);
+            var last7 = orders1.Where(o => o.date >= lastWeek).ToList();
+
+            foreach (var order in last7)
+            {
+                Console.WriteLine($"Order Date: {order.date}, Amount: {order.Amount}");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
